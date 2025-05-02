a881974c9b2424300349ec7d5e6242334caa96d5
rsh <randrianirinahernandezstephan@gmail.com>
2025-05-02 13:37:19 +0300

    feat: before the chaos

--- init.lua
+++ init.lua
@@ -17,7 +17,7 @@ local lazy_config = require "configs.lazy"
 require("lazy").setup({
   {
     "NvChad/NvChad",
-    lazy = false,
+    lazy = true,
     branch = "v2.5",
     import = "nvchad.plugins",
   },
@@ -35,13 +35,16 @@ require "nvchad.autocmds"
 vim.schedule(function()
   require "mappings"
 end)
--- lspconfig.lua
+
+vim.lsp.enable('markdown_oxide')
+
 local nvim_lsp = require'lspconfig'
 
--- Example configuration for a language server (e.g., bashls for Bash)
+-- configuration for a language server ( bashls for Bash)
 nvim_lsp.bashls.setup{}
 
---Set up pyright for Python (and Django)
+
+--Set up pyright for Python
 nvim_lsp.pyright.setup{
   setting = {
     python = {
@@ -55,7 +58,7 @@ nvim_lsp.pyright.setup{
   }
 }
 -- You can add more language servers here
--- nvim_lsp.tsserver.setup{} -- For TypeScript
+nvim_lsp.ts_ls.setup{} -- For TypeScript
 -- Set up the HTML language server
 
 -- nvim_lsp.html.setup{
